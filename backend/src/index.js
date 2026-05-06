09d9bdaf7af5c9d55b02e98c2db846351f301ec2
Shobhit Tiwari <officialshobhit108@gmail.com>
2026-05-06 20:41:28 +0530

    fix render deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,13 +30,14 @@ app.use("/api/music", musicRoutes);
 
 
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(_dirname,"../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-})
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
+
 
 server.listen(PORT, (req, res) => {
   console.log(`server is running on port ${PORT} `);
