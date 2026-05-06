39cc8a3cf53fc8c4c130c4109fdf8deb2a08581a
Shobhit Tiwari <officialshobhit108@gmail.com>
2026-05-06 20:58:30 +0530

    fix express production routing

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,12 +1,11 @@
 import { config } from "dotenv"
 import mongoose from "mongoose"
 
- export const dbConnetion =   async () => {
-    try {
-     await   mongoose.connect(process.env.MONGODB_URI )
-     console.log("database is connected ");
-     
-    } catch (error) {
-        console.log("database connection failed")
-    }
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("database is connected");
+  } catch (error) {
+    console.log("database connection failed");
+  }
+};
