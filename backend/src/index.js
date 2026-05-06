d1537804b7a162c8fa2a17b7f47e62c387052474
Shobhit Tiwari <officialshobhit108@gmail.com>
2026-05-06 21:30:41 +0530

    restore music routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
-
+import musicRoutes from "./routes/music.route.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -27,7 +27,7 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
+app.use("/api/music", musicRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
